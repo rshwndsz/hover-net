61404398ffc1269f7911e3c88cd66af771e40ec1
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-12-23 12:44:22 +0530

    Fix encoder!
    * Fix problems in _ResidualUnit and residual_block
    * WiP: Fix some problems in _DenseUnit and dense_blk

--- hovernet/model.py
+++ hovernet/model.py
@@ -2,22 +2,6 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-# Pretrained models
-import segmentation_models_pytorch as smp
-
-# Model pretrained on imagenet
-# See: https://github.com/qubvel/segmentation_models.pytorch/
-model = smp.Unet(encoder_name="resnet34",
-                 encoder_depth=5,
-                 encoder_weights="imagenet",
-                 decoder_use_batchnorm=True,
-                 decoder_channels=[256, 128, 64, 32, 16],
-                 # See: https://arxiv.org/pdf/1808.08127.pdf
-                 decoder_attention_type=None,
-                 activation=None,
-                 in_channels=3,
-                 classes=1
-                 )
 
 
 class _BNRelu(nn.Module):
@@ -30,26 +14,36 @@ class _BNRelu(nn.Module):
 
 
 class _ResidualUnit(nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
+    def __init__(self, in_channels, out_channels, stride, expansion=4, preact=True):
         super(_ResidualUnit, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels//4, kernel_size=1,
-                               stride=stride, padding=1)
-        self.bn_relu1 = _BNRelu(out_channels//4)
-        self.conv2 = nn.Conv2d(out_channels//4, out_channels//4, kernel_size=3,
-                               stride=stride, padding=1)
-        self.conv3 = nn.Conv2d(out_channels//4, out_channels,
-                               kernel_size=1, stride=stride)
-        self.bn_relu2 = _BNRelu(out_channels)
+        self.preact = preact
+        bottleneck_channels = out_channels // expansion
 
-        if stride != 1:
+        self.bn_relu1 = _BNRelu(in_channels)
+
+        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1,
+                               stride=1, padding=0, dilation=1, bias=False)
+        self.bn_relu2 = _BNRelu(bottleneck_channels)
+
+        self.conv2 = nn.Conv2d(bottleneck_channels, bottleneck_channels, kernel_size=3,
+                               stride=stride, padding=1, dilation=1, bias=False)
+        self.bn_relu3 = _BNRelu(bottleneck_channels)
+
+        self.conv3 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1,
+                               stride=1, padding=0, dilation=1, bias=False)
+
+        if in_channels != out_channels:
             self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1,
-                                      stride=stride)
+                                      stride=stride, padding=0, dilation=1, bias=False)
+        else:
+            self.shortcut = nn.Sequential()
 
     def forward(self, inputs):
-        out = self.bn_relu1(self.conv1(inputs))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else out
-        out = self.bn_relu1(self.conv2(out))
-        out = self.bn_relu2(self.conv3(out))
+        out = self.bn_relu1(inputs) if self.preact else inputs
+        shortcut = self.shortcut(inputs)
+        out = self.bn_relu2(self.conv1(out))
+        out = self.bn_relu3(self.conv2(out))
+        out = self.conv3(out)
         out += shortcut
 
         return out
@@ -58,36 +52,43 @@ class _ResidualUnit(nn.Module):
 class _DenseUnit(nn.Module):
     def __init__(self, in_channels):
         super(_DenseUnit, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 128, kernel_size=1)
+        self.conv1 = nn.Conv2d(in_channels, 128, kernel_size=1,
+                               padding=0, stride=1, dilation=1, bias=False)
         self.bn_relu1 = _BNRelu(128)
-        self.conv2 = nn.Conv2d(128, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(128, 32, kernel_size=5,
+                               padding=2, stride=1, dilation=1, bias=False)
         self.bn_relu2 = _BNRelu(32)
 
     def forward(self, inputs):
         out = self.bn_relu1(self.conv1(inputs))
         out = self.bn_relu2(self.conv2(out))
 
-        return torch.cat([out, F.upsample(inputs)])
+        return torch.cat([out, F.interpolate(inputs, size=out.shape)])
 
 
 class _Encoder(nn.Module):
-    def __init__(self, channels):
+    def __init__(self):
         super(_Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(channels[0], channels[0],
-                               kernel_size=7, padding=3)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
+                               padding=3, stride=1, dilation=1, bias=False)
         self.residual_block1 = nn.Sequential(
-            *[_ResidualUnit(channels[0], channels[1], stride=1), ] * 3
+            _ResidualUnit(64, 256, stride=1, preact=False),
+            *[_ResidualUnit(256, 256, stride=1, preact=True), ] * 2
         )
         self.residual_block2 = nn.Sequential(
-            *[_ResidualUnit(channels[1], channels[2], stride=2), ] * 4
+            _ResidualUnit(256, 512, stride=2, preact=False),
+            *[_ResidualUnit(512, 512, stride=1, preact=True), ] * 3
         )
         self.residual_block3 = nn.Sequential(
-            *[_ResidualUnit(channels[2], channels[3], stride=4), ] * 6
+            _ResidualUnit(512, 1024, stride=2, preact=False),
+            *[_ResidualUnit(1024, 1024, stride=1, preact=True), ] * 5
         )
         self.residual_block4 = nn.Sequential(
-            *[_ResidualUnit(channels[3], channels[4], stride=8), ] * 3
+            _ResidualUnit(1024, 2048, stride=2, preact=False),
+            *[_ResidualUnit(2048, 2048, stride=1, preact=True), ] * 2
         )
-        self.conv2 = nn.Conv2d(channels[4], channels[5], kernel_size=1)
+        self.conv2 = nn.Conv2d(2048, 1024, kernel_size=1,
+                               padding=0, stride=1, dilation=1, bias=False)
 
     def forward(self, inputs):
         x = self.conv1(inputs)
@@ -95,7 +96,6 @@ class _Encoder(nn.Module):
         x = self.residual_block2(x)
         x = self.residual_block3(x)
         x = self.residual_block4(x)
-        x = self.residual_block5(x)
         x = self.conv2(x)
 
         return x
@@ -104,33 +104,36 @@ class _Encoder(nn.Module):
 class _Decoder(nn.Module):
     def __init__(self, input_shape, in_channels):
         super(_Decoder, self).__init__()
-        self.upsample = nn.Upsample(size=input_shape)
-        self.conv1 = nn.Conv2d(in_channels, 256,
-                               kernel_size=5)
+        self.input_shape = input_shape
+
+        self.conv1 = nn.Conv2d(in_channels, 256, kernel_size=5,
+                               padding=2, stride=1, dilation=1, bias=False)
         self.dense_block1 = nn.Sequential(
-            *[_DenseUnit(in_channels), ] * 8
+            _DenseUnit(256),
+            *[_DenseUnit(32), ] * 7
         )
-        self.conv2 = nn.Conv2d(32, 512,
-                               kernel_size=1, stride=1)
-        self.conv3 = nn.Conv2d(512, 128,
-                               kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 512, kernel_size=1,
+                               padding=0, stride=1, dilation=1, bias=False)
+        self.conv3 = nn.Conv2d(512, 128, kernel_size=5,
+                               bias=False)
         self.dense_block2 = nn.Sequential(
-            *[_DenseUnit(in_channels), ] * 4
+            _DenseUnit(128),
+            *[_DenseUnit(32), ] * 3
         )
-        self.conv4 = nn.Conv2d(32, 128,
-                               kernel_size=1)
-        self.conv5 = nn.Conv2d(128, 256,
-                               kernel_size=5)
-        self.conv6 = nn.Conv2d(256, 64,
-                               kernel_size=1)
+        self.conv4 = nn.Conv2d(32, 128, kernel_size=1,
+                               padding=0, stride=1, dilation=1, bias=False)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=5,
+                               padding=2, stride=1, dilation=1, bias=False)
+        self.conv6 = nn.Conv2d(256, 64, kernel_size=1,
+                               padding=0, stride=1, dilation=1, bias=False)
 
     def forward(self, inputs):
-        x = self.upsample(inputs)
+        x = F.interpolate(inputs, size=self.input_shape)
         x = self.conv1(x)
         x = self.dense_block1(x)
-        x = self.conv3(self.upsample(self.conv2(x)))
+        x = self.conv3(F.interpolate(self.conv2(x), size=self.input_shape))
         x = self.dense_block2(x)
-        x = self.conv5(self.upsample(self.conv4(x)))
+        x = self.conv5(F.interpolate(self.conv4(x), size=self.input_shape))
         x = self.conv6(x)
 
         return x
@@ -139,9 +142,9 @@ class _Decoder(nn.Module):
 class HoverNet(nn.Module):
     def __init__(self, input_shape, in_channels):
         super(HoverNet, self).__init__()
-        self.encoder = _Encoder([in_channels, 256, 512, 2048, 1024])
-        self.decoder_np = _Decoder(input_shape, in_channels)
-        self.decoder_hv = _Decoder(input_shape, in_channels)
+        self.encoder = _Encoder()       # <<< Change: Hardcoded
+        self.decoder_np = _Decoder(input_shape, 1024)
+        self.decoder_hv = _Decoder(input_shape, 1024)
 
     def forward(self, inputs):
         x = self.encoder(inputs)
